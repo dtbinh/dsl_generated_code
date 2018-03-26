01914c115b9bf52b1c96c991df1e170125dd015a
eseggioli <eseggioli@gmail.com>
2018-03-26 21:33:08 +0200

    fix up

--- state_machine.py
+++ state_machine.py
@@ -10,7 +10,7 @@ class StateMachine:
  current=None
  def __init__(self,current):
 	self.current=current
-        self.build(self.current)
+        self.build()
 
  def create_initial_state(self,name, role, action_method):
 	state=State(name,role,action_method,self.current)
@@ -37,7 +37,7 @@ class StateMachine:
 		 
        else: 
 	    set_current_state(self,new_state,state.next)
- def build(self,current):
+ def build(self):
       self.create_initial_state("s1","fire_locator",sim.find_fire).create_state("s2","fire_locator",sim.propagate_fire_position).create_state("s3","fire_fighters",sim.go_to_fire_location).create_state("s4","fire_locator",sim.tend_away_from_fire).create_state("s5","fire_fighters",sim.turn_down_fire)
       for state in self.states:
       	state.execute()
